ef760b7c09c2d85639fb82dfe34e23f0709a4ec8
rmyau <91686883+rmyau@users.noreply.github.com>
2022-03-12 00:50:08 +0300

    Добавлен код для 11 задачи

--- LR5/Program.fs
+++ LR5/Program.fs
@@ -2,7 +2,17 @@
 
 open System
 
+//c 11
+let Answer11 = 
+    Console.WriteLine("Какой у Вас любимый язык?")
+    let ans=
+        Console.ReadLine()
+    if (ans.Trim()="F#" || ans.Trim()="Prolog") then
+        Console.WriteLine("Подлиза!")
+    else 
+        Console.WriteLine("Интересно!")
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    
     0 // return an integer exit code
