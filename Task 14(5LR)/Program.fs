cfcc2a126f0dbe89122ef2ea759db81794c007ee
rmyau <91686883+rmyau@users.noreply.github.com>
2022-03-15 23:30:28 +0300

    Добавлено 14 задание из 5 лр

---
+++ Task 14(5LR)/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let Del n f init = 
+    let rec FDel n f init nowN = 
+        if nowN=0 then init
+        else 
+            let NextInit=
+                if n%nowN=0 then f init nowN
+                else init
+            let newN=nowN-1
+            FDel n f NextInit newN
+    FDel n f init n            
+[<EntryPoint>]
+let main argv =
+    let n=Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine(Del n (fun x y -> x*y) 1)
+    0 // return an integer exit code
