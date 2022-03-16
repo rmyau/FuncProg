77289ddbbe456e6fd94832bfbf27d62715efc818
rmyau <91686883+rmyau@users.noreply.github.com>
2022-03-17 00:31:48 +0300

    Добавлен 3-й метод в 18 задании в 5лр

--- Task 18 (5LR)/Program.fs
+++ Task 18 (5LR)/Program.fs
@@ -36,6 +36,28 @@ let NumOddCifr n =
             let NextN = n/10
             NumCifr NextN nextNum
     NumCifr n 0
+
+//М3: произведение делителей числа, сумма цифр которых меньше сумма цифр исходного числа
+let SumCifr n = 
+    let rec Sum n init = 
+        if n =0 then init
+        else 
+            let nextInit = init+(n%10)
+            let nextN=n/10
+            Sum nextN nextInit
+    Sum n 0
+
+let MultDel n = 
+    let rec MultDelCifr n result del = 
+        if del=1 then result
+        else 
+            let nextRes= 
+                if n%del=0 && (SumCifr del < SumCifr n) then result*del
+                else result
+            let nextDel = del-1
+            MultDelCifr n nextRes nextDel
+    MultDelCifr n 1 n
+
 [<EntryPoint>]
 let main argv =
     let res1 = SumDelPrime 6
@@ -43,4 +65,9 @@ let main argv =
 
     let res2=NumOddCifr 1234567
     Console.WriteLine("Кол-во нечетных цифр числа, которые больше 3 для числа 1234567 = {0}",res2)
+
+    //del(32) = {32,16,8,4,2,1}
+    let res3=MultDel 32
+    Console.WriteLine("Произведение делителей 32, сумма цифр которых меньше суммы цифр 32 = {0}",res3)//4*2
+
     0 // return an integer exit code
