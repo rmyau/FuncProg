36ef50f5a1f84397885499172e880a8a5822c34c
rmyau <91686883+rmyau@users.noreply.github.com>
2022-03-16 21:33:17 +0300

    Сделано 15 задание из 5 лр

---
+++ Task 15(5LR)/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let NOD x y =
+    let rec RNod x y del = 
+        if del = 1 then true
+        else 
+            if (x%del=0 && y%del=0) then false
+            else RNod x y (del-1)
+    if x>y then RNod x y y
+    else RNod x y x
+
+let RoundPrimeNum n f init = 
+    let rec PrimeNum n f init num = 
+        if num = 0 then init
+        else 
+            let nextInit=
+                if (NOD n num) = true then (f init num)
+                else init
+            let nextNum = num-1
+            PrimeNum n f nextInit nextNum
+    PrimeNum n f init n
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine(NOD 11 9);
+    Console.WriteLine(RoundPrimeNum 6 (fun x y -> x+y) 0)
+    0 // return an integer exit code
