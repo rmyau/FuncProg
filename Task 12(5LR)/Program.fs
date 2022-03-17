539546922371588c2c20aeb246c825b4e4f27d14
rmyau <91686883+rmyau@users.noreply.github.com>
2022-03-17 22:33:32 +0300

    Делала час 12 задание, потому что табуляция - зло!!!

--- Task 12(5LR)/Program.fs
+++ Task 12(5LR)/Program.fs
@@ -4,5 +4,21 @@ open System
 
 [<EntryPoint>]
 let main argv =
+    let Answer (lang:string)=
+        match lang.Trim() with
+        |"F#"|"Prolog" -> "Подлиза!"
+        |"C++" -> "Здорово!"
+        |"Python"->"Похоже, ты не ищешь сложных путей!)"
+        |"C#" -> "А ты крутой!"
+        |"Pascal" -> "Сразу видно - интеллектуал!"
+        |_ ->"Интересно!" 
+    //Суперпозиция
+    Console.WriteLine("Какой у Вас любимый язык?")
+    (Console.ReadLine>>Answer>>Console.WriteLine)()
+   //Каррирование
+    Console.WriteLine("Какой у Вас любимый язык?")
+    let result answer func out = out(func(answer()))
+    result Console.ReadLine Answer Console.WriteLine
+    0 
+
    
-    0 // return an integer exit code
