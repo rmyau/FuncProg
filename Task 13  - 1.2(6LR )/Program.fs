7eefe3f3e438d2389a5ee267a845d2ce514e6be0
rmyau <91686883+rmyau@users.noreply.github.com>
2022-03-18 10:54:50 +0300

    Сделано 13 задание из 6лр

---
+++ Task 13  - 1.2(6LR )/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+//1.2 Найти индекс минимального элемента
+let FindMin lint = 
+    let rec MinEl lint min =
+        match lint with
+        |[]->min
+        |h::tail -> 
+            let newMin =
+                if h<min then h
+                else min
+            MinEl tail newMin
+    MinEl lint lint.Head
+
+[<EntryPoint>]
+let main argv =
+    let l=readData
+    Console.WriteLine("Индекс минимального элемента списка: ")
+    Console.WriteLine(List.findIndex (fun x -> x=FindMin l) l)
+    //lint[8]={1,5,10,2,6,10,1,2}
+    0 // return an integer exit code
