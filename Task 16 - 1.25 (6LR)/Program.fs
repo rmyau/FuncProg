dbb0ccca44028c3b63e503c89c6f08be39051f6c
rmyau <91686883+rmyau@users.noreply.github.com>
2022-03-18 12:57:22 +0300

    Доделана задача 16 из 6 ЛР

---
+++ Task 16 - 1.25 (6LR)/Program.fs
@@ -0,0 +1,33 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+//1.25 Дан массив и интервал. Найти максимальный из элементов в этом интервале
+let MaxInInt (a,b) lint= 
+    let rec InInt lint (a,b) max = 
+        match lint with
+        |[]->max
+        |h::tail-> 
+            let newMax = 
+                if h>max && h>a && h<b then h
+                else max
+            InInt tail (a,b) newMax
+    InInt lint (a,b) a
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите границы интервала")
+    let interval = (Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()))
+    readData |> MaxInInt interval|> Console.WriteLine
+    0 // return an integer exit code
