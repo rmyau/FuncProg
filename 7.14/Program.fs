17cfd45e2a905515f1855504cf60120f328a4047
rmyau <91686883+rmyau@users.noreply.github.com>
2022-03-26 08:30:52 +0300

    Добавлено 14 задание из 7ЛР

---
+++ 7.14/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Program
+
+//1.31 Дан целочисленный массив. Найти количество чётных элементов.
+let ChetNum list = List.length (List.filter (fun x -> x%2=0) list) 
+[<EntryPoint>]
+let main argv =
+
+    readData |>ChetNum|>Console.WriteLine
+    0 // return an integer exit code
