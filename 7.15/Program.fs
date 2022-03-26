f60ef44bc00c927903016e567b02766f0e885b37
rmyau <91686883+rmyau@users.noreply.github.com>
2022-03-26 09:25:17 +0300

    Добавлено 15 задание из 7 ЛР

--- 7.15/Program.fs
+++ 7.15/Program.fs
@@ -1,7 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open Program
+//open Program
+let rec readList n =
+    List.init(n) (fun index->Console.ReadLine()|>Convert.ToDouble)
+
+let readData = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
 //1.41 Дан целочисленный массив. Найти среднее арифметическое модулей его элементов.
 let AverageAbsList list = List.average (List.map (fun (x:float)-> Math.Abs(x)) list )
 [<EntryPoint>]
