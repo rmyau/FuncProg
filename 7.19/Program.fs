f0f9f9db5098a8e720b714094ef903cf5075fcc5
rmyau <91686883+rmyau@users.noreply.github.com>
2022-04-08 19:06:22 +0300

    Добавлен 2-1 метод 19 задачи 7ЛР

--- 7.19/Program.fs
+++ 7.19/Program.fs
@@ -1,6 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+
+let rec writelist list=
+    List.iter(fun x->printfn "%O" x) list
 //1 Дана строка. Необходимо найти общее количество русских символов.
 let numRus str = String.length (String.filter (fun x -> x>='А' && x<='я') str)
 //9 Дана строка. Необходимо проверить образуют ли строчные
@@ -24,19 +27,21 @@ let findData str =
         |_-> 
             let newstr = 
                 if strNow.Length<10 then
-                    strNow+strBase.Remove(1 ,(strBase.Length -2)) String.
-                else (strNow.Remove 0)+ strBase[0]
+                    strNow + strBase.Remove(1,strBase.Length-1)
+                else strNow.Remove(0,1)+(strBase.Remove(1,strBase.Length-1))
             let newList = 
-                if newstr.Length = 10 && (newstr>"00.00.0000" && newstr<"31.12.9999") then strList @ [newstr]
+                if (newstr.Length = 10 && (newstr>"00.00.0000" && newstr<"31.12.9999")) then strList @ [newstr]
                 else strList
-            date (strBase.Remove 0) newstr newList
+            date (strBase.Remove(0,1)) newstr newList
     date str "" []
 
 [<EntryPoint>]
 let main argv =
     let str = Console.ReadLine()
-    numRus str |>printf "Кол-во русских символов в строке: %O\n"
-    isPalindrom str|>printf "Образуют ли строчные буквы палиндром: %O"
-
+ 
+    numRus str |>printfn "Кол-во русских символов в строке: %O"
+    isPalindrom str|>printfn "Образуют ли строчные буквы палиндром: %O"
+    printfn "Даты найденные в тексте"
+    findData str|> writelist
 
     0 // return an integer exit code
