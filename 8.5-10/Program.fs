84649bfd021a3156a546e59e8d4865048efe1075
rmyau <91686883+rmyau@users.noreply.github.com>
2022-05-27 22:39:31 +0300

    task 5

---
+++ 8.5-10/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open System.Text.RegularExpressions;
+type Passport (name: string, surname: string, number: int, series:int,birthday: string,birthPlace: string ) =
+    member this.name = name
+    member this.surname = surname
+    member this.number = number
+    member this.series = series
+    member this.birthday = birthday
+    member this.birthPlace = birthPlace
+
+
+
+[<EntryPoint>]
+let main argv =
+    
+    0 // return an integer exit code
