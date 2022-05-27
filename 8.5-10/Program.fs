13f76d19aa851537d3660cc8d5cd45a7b3e8bf3d
rmyau <91686883+rmyau@users.noreply.github.com>
2022-05-27 22:48:55 +0300

    task 6

--- 8.5-10/Program.fs
+++ 8.5-10/Program.fs
@@ -10,9 +10,11 @@ type Passport (name: string, surname: string, number: int, series:int,birthday: 
     member this.birthday = birthday
     member this.birthPlace = birthPlace
 
+    override this.ToString() = $"Passport name: {this.name}, surname: {this.surname}, number: {this.number}, series: {this.series}, birthday: {this.birthday}, birthPlace: {this.birthPlace}."
 
 
 [<EntryPoint>]
 let main argv =
-    
+    let p = Passport("Егор","Иванов",0313, 455544, "09.09.3000", "ghjhfjfhjf")
+    printfn "%O" (p.ToString())
     0 // return an integer exit code
