c46b7526ad7dc56d45684efc576662809070d50e
rmyau <91686883+rmyau@users.noreply.github.com>
2022-05-27 23:41:32 +0300

    task 8

--- 8.5-10/Program.fs
+++ 8.5-10/Program.fs
@@ -3,12 +3,46 @@
 open System
 open System.Text.RegularExpressions;
 type Passport (name: string, surname: string, number: int, series:int,birthday: string,birthPlace: string ) =
-    member this.name = name
-    member this.surname = surname
-    member this.number = number
-    member this.series = series
-    member this.birthday = birthday
-    member this.birthPlace = birthPlace
+    member this.name 
+        with get() = name
+        and set(newName:string) =
+            if (Regex.IsMatch(newName, "^\\s*[A-ZА-Я][а-яa-z]+\\s*$"))
+            then this.name<-newName
+            else printf"Данные некорректны"
+
+          
+    member this.surname 
+        with get() = surname 
+        and set(newSurname:string) = 
+            if (Regex.IsMatch(newSurname, "^\\s*[A-ZА-Я][а-яa-z]+\\s*$"))
+            then this.surname<-newSurname
+            else printf"Данные некорректны"
+
+    member this.number  
+        with get() = number
+        and set(num:int) = 
+            if (Regex.IsMatch(num.ToString(), "^\\s*[0-9]{6}\\s*$"))
+            then this.number<-num
+            else printf"Данные некорректны"
+
+    member this.series 
+        with get() = series
+        and set(seria:int) = 
+            if (Regex.IsMatch(seria.ToString(), "^\\s*[0-9]{4}\\s*$"))
+            then this.series<-seria
+            else printf"Данные некорректны" 
+    member this.birthday 
+        with get() = birthday
+        and set(birth: string) = 
+            if (Regex.IsMatch(birth.ToString(), "^\\s*\\d{2}\\.\\d{2}\\.\\d{4}\\s*$"))
+            then this.birthday <- birth
+            else printf"Данные некорректны" 
+    member this.birthPlace 
+        with get() = birthPlace
+        and set(place:string) = 
+            if (Regex.IsMatch(place, "^\\s*[A-ZА-Я][а-яa-z]+\\s*$"))
+            then this.birthPlace <- place
+            else printf"Данные некорректны"
 
     override this.Equals(pas) = 
         match pas with
