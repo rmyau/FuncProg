d12f8f3a57332e267ba0c717287d1c18719d56e5
rmyau <91686883+rmyau@users.noreply.github.com>
2022-05-28 00:57:41 +0300

    task 10  - set and binarylist are slower

--- 8.5-10/Program.fs
+++ 8.5-10/Program.fs
@@ -1,7 +1,8 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
-open System.Text.RegularExpressions;
+open System.Text.RegularExpressions
+open System.Diagnostics
 type Passport (name: string, surname: string, number: int, series:int,birthday: string,birthPlace: string ) =
     member this.name 
         with get() = name
@@ -73,8 +74,8 @@ type PassportArray(arr: Passport array) =
 
 type PassportList(list: Passport list) = 
     inherit DocCollection()
-    member this.list = list
-    override this.searchDoc(passport) = 
+    member this.list: (Passport list) = list
+    override this.searchDoc(passport:Passport) = 
         List.exists (fun x -> x.Equals passport) this.list
 
 type PassportBinList(binList: Passport list ) = 
@@ -90,7 +91,7 @@ type PassportBinList(binList: Passport list ) =
             |1->BinSearch list.[..mid - 1] p
             |_ -> BinSearch list.[mid + 1..] p
 
-    member this.binList = List.sortBy(fun (pas:Passport) -> (pas.number,pas.series)) List
+    member this.binList = List.sortBy(fun (pas:Passport) -> (pas.number,pas.series)) binList
           
     override this.searchDoc(passport) = 
         BinSearch this.binList passport
@@ -102,10 +103,56 @@ type PassportSet(list: Passport list) =
         Set.contains passport this.set
 
 
+let charsForRandom = "абвгдежзиклмнопрстуфхчшщьыъэюя"
+let randomStr len =
+    let random = Random()
+    let randomChars = [|for i in 0..len -> charsForRandom.[random.Next(charsForRandom.Length)]|]
+    new System.String(randomChars)
+
+let randPassport () = 
+    let r = Random()
+    let name = randomStr (r.Next(5,11))
+    let surname = randomStr (r.Next(5,11))
+    let number = r.Next(100000,999999)
+    let series = r.Next(1000,9999)
+    let birthday = r.Next(30).ToString()+"."+r.Next(1,12).ToString()+"."+r.Next(1925,2010).ToString()
+    let place = randomStr (r.Next(10,20))
+    Passport(name,surname,number,series, birthday,place)
+let randPasList(len) = 
+   [for i in 0..len -> randPassport() ]
     
 
 [<EntryPoint>]
 let main argv =
     let p = Passport("Егор","Иванов",0313, 455544, "09.09.3000", "ghjhfjfhjf")
-    printfn "%O" (p.ToString())
-    0 // return an integer exit code
+    let passportList = randPasList (1000)
+
+    let list = PassportList (passportList)
+    let array = PassportArray(List.toArray passportList)
+    let set = PassportSet(passportList)
+    let binarylist = PassportBinList(passportList)
+    
+    let time = new Stopwatch()
+
+    time.Restart()
+    list.searchDoc(p)
+    time.Stop()
+    printfn "time list - %O" time.Elapsed
+
+    time.Restart()
+    array.searchDoc(p)
+    time.Stop()
+    printfn "time array - %O" time.Elapsed
+
+    time.Restart()
+    set.searchDoc(p)
+    time.Stop()
+    printfn "time set - %O" time.Elapsed
+
+    time.Restart()
+    binarylist.searchDoc(p)
+    time.Stop()
+    printfn "time binary list - %O" time.Elapsed
+
+    
+    0 // return an integer exit 
